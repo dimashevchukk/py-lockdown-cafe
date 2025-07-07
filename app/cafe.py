dc9093df8178e236b646ba700c5e2c9a504dfb4c
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-07-07 22:07:08 +0300

    comments removed

--- app/cafe.py
+++ app/cafe.py
@@ -13,7 +13,6 @@ class Cafe:
             raise NotVaccinatedError("Visitor must be vaccinated")
 
         vaccine_expiration_date = visitor.get("vaccine").get("expiration_date")
-        # vaccine_expiration_date = visitor["vaccine"]["expiration_date"]
         if vaccine_expiration_date < datetime.date.today():
             raise OutdatedVaccineError("Vaccine is outdated")
 
